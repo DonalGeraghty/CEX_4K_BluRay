62c7278af4c32dbce9a5d7d748c8bde57a3fdda7
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2025-04-09 21:27:51 +0100

    initial commit

---
+++ my_script.py
@@ -0,0 +1,12 @@
+import cex
+from playwright.sync_api import sync_playwright
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        cex.test_cex(page)
+        browser.close()
+
+if __name__ == "__main__":
+        main()
