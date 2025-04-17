45ae78f7eaa03581314a57ef7dca59769717c890
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2025-04-17 23:35:37 +0100

    adding check for file yesterday

--- actions/manipulate.py
+++ actions/manipulate.py
@@ -5,43 +5,49 @@ import pandas as pd
 
 
 class Manipulate:
-    def get_today_file(self):
+    def get_today_filename(self):
         today_date = date.today()
         filetype = '.csv'
         return str(today_date) + filetype
 
-    def get_yesterday_file(self):
+    def get_yesterday_filename(self):
         today_date = date.today()
         yesterday_date = today_date - timedelta(days=1)
         filetype = '.csv'
         return str(yesterday_date) + filetype
 
-    def get_data_folder_path(self):
-        path = ''
-        return path
-
     def return_diff_yesterday_today(self):
         column = "name"
 
-        yesterday_list = self.get_list_from_csv_column(self.get_data_folder_path() + self.get_yesterday_file(), column)
-        today_list = self.get_list_from_csv_column(self.get_data_folder_path() + self.get_today_file(), column)
+        yesterday_list = self.get_list_from_csv_column(self.get_yesterday_filename(), column)
+        today_list = self.get_list_from_csv_column(self.get_today_filename(), column)
 
         # Elements in today but not in yesterday
         difference = list(set(today_list) - set(yesterday_list))
         return difference
 
     def get_prices_from_list(self, filter_list):
-        df = pd.read_csv(self.get_data_folder_path() + self.get_today_file())
+        df = pd.read_csv(self.get_today_filename())
         filtered_df = df[df['name'].isin(filter_list)]
         result_df = filtered_df[['name', 'price']]
         return result_df
 
-    def get_list_from_csv_column(self, name: str, column: str):
-        df = pd.read_csv(name)
-        return df[column].tolist()
+    def check_file_exists(self, file_path):
+        return os.path.exists(file_path)
+
+    def get_list_from_csv_column(self, filename: str, column: str):
+        exists = self.check_file_exists(filename)
+        if exists:
+            df = pd.read_csv(filename)
+            return df[column].tolist()
+        else:
+            headers = {'id': [], 'name': [], 'price': []}
+            df = pd.DataFrame(headers)
+            df.to_csv(filename, index=False)
+            return df[column].tolist()
 
     def output_blu_rays(self, blu_ray_all):
-        filename = self.get_data_folder_path() + self.get_today_file()
+        filename = self.get_today_filename()
         headers = ["id", "name", "price"]
         print(filename)
         with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
@@ -56,8 +62,8 @@ class Manipulate:
         return script_dir
 
     def cleanup_files(self):
-        file_today = self.get_today_file()
-        file_yesterday = self.get_yesterday_file()
+        file_today = self.get_today_filename()
+        file_yesterday = self.get_yesterday_filename()
         self.delete_other_csvs(file_today, file_yesterday)
 
     def delete_other_csvs(self, file1: str, file2: str):
